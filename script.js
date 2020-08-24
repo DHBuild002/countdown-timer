3b206d0a183182dc54b65d45b914cb35ec9978df
Dan Haddock <dhbuild.002@gmail.com>
2020-08-24 13:41:04 +0100

    Countdown Timer - Completed - First Commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Connected")
+
+function start(){
+	let inputTime = document.getElementById("input").value
+
+	if(inputTime != 0){
+		document.getElementById("countdownClock").innerHTML = inputTime
+		let timer = setInterval(()=>{ if( inputTime != 0){ document.getElementById("countdownClock").innerHTML = --inputTime}
+		else {
+			clearInterval(timer);
+			clearHTML();
+			}
+		}, 1000);
+	} else {
+	document.getElementById("input").innerHTML = "Please Enter a Time First"
+	timer.clearInterval();
+	}
+}
+let clearHTML = () => {
+	document.getElementById("countdownClock").innerHTML = "Timer Finished"
+}
+let btn = document.getElementById('enterTime');
+btn.addEventListener('click', function(){
+	console.log('New Time Entered');
+	start();
+})
+// Main Chunk of code sourced from here: https://stackoverflow.com/a/48343641/7206229
