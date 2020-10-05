04e1c2e55a8eea84aa14f1c71e80a8b50e937542
Dan Haddock <dhbuild.002@gmail.com>
2020-10-05 21:42:40 +0100

    No change to JS file, styling tweaks and better controls hierarchy added

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-console.log("Connected")
+const start = () => {
+  let inputTime = document.getElementById("input").value;
 
-function start(){
-	let inputTime = document.getElementById("input").value
+  if (inputTime != 0) {
+    document.getElementById("countdownClock").innerHTML = inputTime;
 
-	if(inputTime != 0){
-		document.getElementById("countdownClock").innerHTML = inputTime
-		let timer = setInterval(()=>{ if( inputTime != 0){ document.getElementById("countdownClock").innerHTML = --inputTime}
-		else {
-			clearInterval(timer);
-			clearHTML();
-			}
-		}, 1000);
-	} else {
-	document.getElementById("input").innerHTML = "Please Enter a Time First"
-	timer.clearInterval();
-	}
-}
-let clearHTML = () => {
-	document.getElementById("countdownClock").innerHTML = "Timer Finished"
-}
-let btn = document.getElementById('enterTime');
-btn.addEventListener('click', function(){
-	console.log('New Time Entered');
-	start();
-})
+    let timer = setInterval(() => {
+      if (inputTime != 0) {
+        document.getElementById("countdownClock").innerHTML = --inputTime;
+      } else {
+        clearInterval(timer);
+        clearHTML();
+      }
+    }, 1000);
+  }
+};
+const clearHTML = () => {
+  document.getElementById("input").value = "";
+  // document.getElementById("countdownClock").innerHTML = ''
+  document.getElementById(
+    "countdownClock"
+  ).innerHTML = `<h3>Timer Complete.</h3> 
+					<p>Please enter a new time to begin again.</p>`;
+};
+let btn = document.getElementById("enterTime");
+btn.addEventListener("click", () => {
+  console.log("New Time Entered");
+  start();
+});
 // Main Chunk of code sourced from here: https://stackoverflow.com/a/48343641/7206229
+
+const countUp = () => {
+  let inputUp = document.getElementById("inputUp").value;
+
+  if (n > 1) {
+    return [];
+  } else {
+    const countdownArr = countUp(n - 1);
+    countdownArr.splice(0, 0, n);
+    return n;
+  }
+};
+
+const buttonStart = document.getElementById("startCount");
+buttonStart.addEventListener("click", () => {
+  console.log("Enter a time to countdown from");
+  countUp();
+});
