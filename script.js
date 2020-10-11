eb972a93100fbd685438b4086cbe9271e4d20faa
Dan Haddock <dhbuild.002@gmail.com>
2020-10-11 21:32:44 +0100

    Attempt 1 - Trying to fix countup over 10 bug

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ const countDown = () => {
 // Main Chunk of code sourced from here: https://stackoverflow.com/a/48343641/7206229
 
 const countUp = () => {
+  let interval;	
   let inputTime = document.getElementById("input").value; // 5
   console.log(inputTime);
 
@@ -27,20 +28,24 @@ const countUp = () => {
     document.getElementById("countupClock").innerHTML = 0;
     let run = () => { // 5 > 0
 	  let clock = document.getElementById("countupClock");
-	  if(clock.innerHTML)
-      clock.value = clock.value < inputTime ? clock.value++ : inputTime;
-      clock.innerHTML = clock.value;
-     }
-    setInterval(run(), 3000);
-    } else {
-      clearInterval(run);
-    };
+	  if(clock.innerHTML < inputTime) {
+		  clock.innerHTML++;
+		  console.log('Counter Value: ', clock.innerHTML)
+     } else {
+	  clearInterval(interval);
+	  console.log('Cleared Interval');
+	  //clearHTML();
+		}
+	};
+	interval = setInterval(run, 1000)	
   }
+}
+// Solution courtesy of S.Prathare: 
+// https://stackoverflow.com/questions/64304558/countup-function-failing-to-increment-from-0-to-inputted-time/64304995#64304995
 
 // Revert CD Window in UI back to empty 
 const clearHTML = () => {
   document.getElementById("input").value = "";
-  // document.getElementById("countdown/upClock").innerHTML = ''
   document.getElementById("countupClock").innerHTML = `<h3>Timer Complete.</h3> 
 					       <p>Please enter a new time to begin again.</p>`;
 };
@@ -49,6 +54,5 @@ const clearHTML = () => {
 let btn = document.getElementById("enterTime");
 
 btn.addEventListener("click", () => {
-  console.log("New Time Entered");
-  countUp();
+   countUp();
 });
