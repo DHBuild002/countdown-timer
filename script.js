6167ae6ed14e30dfb9758a16e1ec745451949a39
Dan Haddock <dhbuild.002@gmail.com>
2020-10-11 22:16:39 +0100

    Bug still present - Unable to inc higher than 9

--- script.js
+++ script.js
@@ -20,30 +20,62 @@ const countDown = () => {
 // Main Chunk of code sourced from here: https://stackoverflow.com/a/48343641/7206229
 
 const countUp = () => {
-  let interval;	
+  let interval;
   let inputTime = document.getElementById("input").value; // 5
   console.log(inputTime);
 
-  if (inputTime != 0) { // 5 != 0
+  if (inputTime != 0) {
+    // 5 != 0
     document.getElementById("countupClock").innerHTML = 0;
-    let run = () => { // 5 > 0
-	  let clock = document.getElementById("countupClock");
-	  if(clock.innerHTML < inputTime) {
-		  clock.innerHTML++;
-		  console.log('Counter Value: ', clock.innerHTML)
-     } else {
-	  clearInterval(interval);
-	  console.log('Cleared Interval');
-	  //clearHTML();
-		}
-	};
-	interval = setInterval(run, 1000)	
+
+    let run = () => {
+      // 5 > 0
+      let clock = document.getElementById("countupClock");
+      if (clock.innerHTML < inputTime) {
+        clock.innerHTML++;
+        console.log("Counter Value: ", clock.innerHTML);
+      } else {
+        clearInterval(interval);
+        console.log("Cleared Interval");
+        clearHTML();
+      }
+    };
+    interval = setInterval(run, 1000);
   }
+};
+
+function count() {
+  var seed = document.getElementById("seed").getAttribute("value");
+  var max = document.getElementById("max").getAttribute("value");
+  var temp = 1;
+  var output = document.getElementById("output");
+  temp = seed;
+  console.log("seed:" + seed);
+  console.log("max:" + max);
+  var id = setInterval(function () {
+    var intdivby2 = temp % 2;
+    var intdivby3 = temp % 3;
+
+    document.getElementById("output").innerHTML +=
+      "remainder of " +
+      temp +
+      " divided by 2 is:" +
+      intdivby2.toString() +
+      "<br>";
+    document.getElementById("output").innerHTML +=
+      "remainder of " +
+      temp +
+      " divided by 3 is:" +
+      intdivby3.toString() +
+      "<br>";
+    temp++;
+    if (temp > max) clearInterval(id);
+  }, 1000);
 }
-// Solution courtesy of S.Prathare: 
+// Solution courtesy of S.Prathare:
 // https://stackoverflow.com/questions/64304558/countup-function-failing-to-increment-from-0-to-inputted-time/64304995#64304995
 
-// Revert CD Window in UI back to empty 
+// Revert CD Window in UI back to empty
 const clearHTML = () => {
   document.getElementById("input").value = "";
   document.getElementById("countupClock").innerHTML = `<h3>Timer Complete.</h3> 
@@ -54,5 +86,5 @@ const clearHTML = () => {
 let btn = document.getElementById("enterTime");
 
 btn.addEventListener("click", () => {
-   countUp();
+  count();
 });
